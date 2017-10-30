0c694bb59482842be51e00b0c8776515686bf54b
shinichi kogai <kogai0121@gmail.com>
2017-10-30 14:25:18 +0900

    tmp

--- src/ast.ml
+++ src/ast.ml
@@ -1,16 +1,11 @@
 (* filename * line * column *)
-type info = INFO of string * int * int | UNKNOWN
+type info = INFO of string * int * int
 [@@deriving show]
 
-let dummyinfo = UNKNOWN
 let createInfo file line column = INFO (file, line, column)
 
 type t =
-  | TermTrue of info
-  | TermFalse of info
-  | TermZero of info
-  | TermSucc of info * t
-  | TermPred of info * t
-  | TermIsZero of info * t
-  | TermIf of info * t * t * t
+  | TermVar of info
+  | TermAbs of info
+  | TermApp of info
 [@@deriving show]
